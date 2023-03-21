8a655264b63e5fdf7e1b64a47e90d7fbaebbb721
hhj <hehaojia1016@163.com>
2023-03-21 16:29:02 +0800

    新增routes

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/dyouwan/utility
 go 1.20
 
 require (
-	github.com/RichardKnop/logging v0.0.0-20190827224416-1a693bdd4fae
-	gopkg.in/yaml.v2 v2.4.0
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.9.0
+	github.com/urfave/negroni v1.0.0
 	gorm.io/driver/mysql v1.4.7
 	gorm.io/gorm v1.24.6
 )
@@ -13,6 +14,5 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
---
+++ routes/routes.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+// Route 路由
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+	Middlewares []negroni.Handler
+}
+
+// AddRoutes 添加路由
+func AddRoutes(routes []Route, router *mux.Router) {
+	var (
+		handler http.Handler
+		n       *negroni.Negroni
+	)
+
+	for _, route := range routes {
+		if len(route.Middlewares) > 0 {
+			n = negroni.New()
+
+			for _, middleware := range route.Middlewares {
+				n.Use(middleware)
+			}
+
+			n.Use(negroni.Wrap(route.HandlerFunc))
+			handler = n
+		} else {
+			handler = route.HandlerFunc
+		}
+
+		router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(handler)
+	}
+}
